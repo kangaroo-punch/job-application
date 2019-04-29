157feee075e499830147cb50e0532bd67373f9dc
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-04-29 15:33:24 -0700

    tdd the form function and got it to pass the test

--- tests/tests.js
+++ tests/tests.js
@@ -1,13 +1,45 @@
 const test = QUnit.test;
 
-test('time to test a function', function(assert) {
+function getApplicant(formData) {
+    const age = formData.get('age') === 'yes';
+    const likeFood = parseInt(formData.get('likeFood'));
+
+    const applicant = {
+        name: formData.get('name'),
+        age: age,
+        cuisine: formData.get('cuisine'),
+        meals: formData.getAll('meals'),
+        likeFood: likeFood
+    };
+    return applicant;
+}
+
+test('return completed form in object', function(assert) {
     //Arrange
     // Set up your parameters and expectations
+    const expected = {
+        name: 'josh',
+        age: true,
+        cuisine: 'indian',
+        meals: ['breakfast', 'lunch'],
+        likeFood: 10,
+
+    };
+
+    const formData = new FormData();
+    formData.set('name', expected.name);
+    formData.set('age', 'yes');
+    formData.set('cuisine', expected.cuisine);
+    formData.set('meals', expected.meals[0]);
+    formData.append('meals', expected.meals[1]);
+    formData.set('likeFood', '10');
+
+
 
     //Act 
     // Call the function you're testing and set the result to a const
-
+    const applicant = getApplicant(formData);
     //Assert
-    assert.equal(true, false);
+    assert.deepEqual(applicant, expected);
 });
  
